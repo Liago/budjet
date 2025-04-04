99e746b6bb418b4dade981e3c1e107f92ca74574
Liago <liago@dev.me>
2025-04-04 15:51:24 +0200

    fix: page container on Notification Page

--- finance-tracker/frontend/web/src/store/index.ts
+++ finance-tracker/frontend/web/src/store/index.ts
@@ -5,6 +5,7 @@ import transactionReducer from "./slices/transactionSlice";
 import dashboardReducer from "./slices/dashboardSlice";
 import recurrentPaymentReducer from "./slices/recurrentPaymentSlice";
 import { createLogger } from "redux-logger";
+import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
 
 // Configurazione redux-logger
 const logger = createLogger({
@@ -48,3 +49,7 @@ export const store = configureStore({
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
+
+// Use throughout your app instead of plain `useDispatch` and `useSelector`
+export const useAppDispatch = () => useDispatch<AppDispatch>();
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
